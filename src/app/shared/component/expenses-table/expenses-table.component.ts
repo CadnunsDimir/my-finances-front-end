765b1f4e9daa92c1292a8551259e7af20816157b
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2022-10-12 17:56:22 -0300

    feat(expenses-table.component): add observables

--- src/app/shared/component/expenses-table/expenses-table.component.ts
+++ src/app/shared/component/expenses-table/expenses-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { arrays } from '../../array.functions';
 import { expenseTypePtBR } from '../../i18n.consts';
 import { Expense } from '../../model/expense.model';
@@ -8,16 +10,15 @@ import { Expense } from '../../model/expense.model';
   templateUrl: './expenses-table.component.html',
   styleUrls: ['./expenses-table.component.scss']
 })
-export class ExpensesTableComponent implements OnInit {
-  
-  private _expenses: Expense[];
-  expensesToPay: number;
-  expensesPaid: number;
-  sumInvestment: number;
+export class ExpensesTableComponent implements OnInit {  
+  private _expenses$ = new BehaviorSubject<Expense[]>([]);
+  expensesToPay$: Observable<number>;
+  expensesPaid$: Observable<number>;
+  sumInvestment$: Observable<number>;
+  expensesOrdered$: Observable<Expense[]>;
 
   @Input() set expenses(expenses: Expense[]) {
-    this._expenses = this.orderByExpirationDayAndIsPaid(expenses);
-    this.sumStats();
+    this._expenses$.next(expenses);
   }
 
   expenseTypePtBR = expenseTypePtBR;
@@ -25,27 +26,28 @@ export class ExpensesTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  get hasValue() {
-    return this._expenses && this._expenses.length;
-  }
-
-  sort() {
-    this._expenses = this.orderByExpirationDayAndIsPaid(this._expenses);
+    this.orderByExpirationDayAndIsPaid();
     this.sumStats();
   }
 
-  private orderByExpirationDayAndIsPaid(expenses: Expense []) {
-    return (expenses || [])
-      .sort((a,b)=> Number(a.expirationDay)-Number(b.expirationDay))
-      .sort((a,b)=> Number(a.isPaid)-Number(b.isPaid));
+  sort() {
+    this._expenses$.next([...this._expenses$.value]);
   }
 
-  private sumStats() {
-    const selectValue = (x: Expense) => x.value;
-    this.expensesToPay = arrays.sum(this._expenses, x=> !x.isPaid, selectValue);
-    this.expensesPaid = arrays.sum(this._expenses, x=> x.isPaid && x.type === 'bill', selectValue);
-    this.sumInvestment = arrays.sum(this._expenses, x=> x.isPaid && x.type === 'investment', selectValue);
+  private orderByExpirationDayAndIsPaid() {
+    this.expensesOrdered$ = this._expenses$.pipe(
+      map(array=> array
+        .sort((a,b)=> Number(a.expirationDay)-Number(b.expirationDay))
+        .sort((a,b)=> Number(a.isPaid)-Number(b.isPaid))));
+  }
+
+  private sumStats() {    
+    this.expensesToPay$ = this.sumExp(x=> !x.isPaid);
+    this.expensesPaid$ = this.sumExp(x=> x.isPaid && x.type === 'bill');
+    this.sumInvestment$ = this.sumExp( x=> x.isPaid && x.type === 'investment');
   };
+
+  private sumExp(predicate: (expense: Expense) => boolean) {
+    return this._expenses$.pipe(map(expenses=> arrays.sum(expenses, predicate, (x: Expense) => x.value)));
+  }
 }
